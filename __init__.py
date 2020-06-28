9f135618e4ff4289ef5da30335426a231174751d
StefanBa <stefanbaumann.b@gmail.com>
2020-06-28 20:39:58 +0200

    testing

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from flask_mail import Mail, Message
 from datetime import datetime
 import os
 
+
 app = Flask(__name__)
 # four forwardslashes would be absolute path, three are relative
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///c_base.db'
