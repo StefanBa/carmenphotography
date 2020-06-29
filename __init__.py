af49a7dfb2394a8536a8696a0dee787d41b3df42
StefanBa <stefanbaumann.b@gmail.com>
2020-06-29 21:39:23 +0200

    first hook steps

--- __init__.py
+++ __init__.py
@@ -92,7 +92,7 @@ def contact2():
 @app.route('/update/', methods=['POST'])
 def update():
     app.logger.warning(request)
-    os.system("cd /var/www/webApp/webApp/ & git pull cp master")
+    os.system("git pull cp master")
     return ("fun2", 200, None)
 
 
