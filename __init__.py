fbb954c701f68b23657255322b5206529be395cd
StefanBa <stefanbaumann.b@gmail.com>
2020-08-04 15:10:37 +0200

    test

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ import traceback
 import mailconfig
 
 app = Flask(__name__)
-# four forwardslashes would be absolute path, three are relative..
+# four forwardslashes would be absolute path, three are relative.
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///c_base.db'
 db = SQLAlchemy(app)
 
