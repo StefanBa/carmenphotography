f2df21ebb64ac823af6fa42ef65dbec7a2db2387
StefanBa <stefanbaumann.b@gmail.com>
2020-08-04 13:16:29 +0200

    webhook

--- __init__.py
+++ __init__.py
@@ -90,7 +90,7 @@ def contact2():
 def update():
     app.logger.warning(request)
     os.system("sudo /home/ubuntu/Desktop/gitpull.sh")
-    return ("webhook ok", 200, None)
+    return ("webhook", 200, None)
 
 if __name__ == "__main__":
     app.run(debug=True)
