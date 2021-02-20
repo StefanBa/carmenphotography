76ea6c8b692b3226980bc3ee2bfc0c0ab16649c4
StefanBa <stefanbaumann.b@gmail.com>
2021-02-20 19:28:50 +0100

    about image

--- bat/transformImages.bat
+++ bat/transformImages.bat
@@ -1,24 +1,25 @@
 @echo off
 cd /d %~dp0
 
-echo delete alle files in static/gallery folder
+echo delete all files in local static/gallery folder
 del ..\static\gallery\*.* /Q
+echo done
+echo.
 
-echo convert all images to requested size and copy them to the static/gallery folder
-
-
+echo convert all images to requested size and copy them to the local static/gallery folder
 for %%a in ("..\galleryOriginal\*jpg") do (
    call scale.bat -source "%%~fa" -target "..\static\gallery\%%~nxa" -max-height 1920 -max-width 1920 -keep-ratio yes -force yes
 )
+echo done
+echo.
 
-
-echo create a txt file with timestamp for cache busting
-
+echo create txt file with timestamp for cache busting
 setlocal
 call :GetUnixTime UNIX_TIME
 echo let image_version = %UNIX_TIME%; > ..\static\gallery\image_version.js
+echo done
+echo.
 goto :copy
-
 
 REM https://stackoverflow.com/questions/11385030/batch-timestamp-to-unix-time
 :GetUnixTime
@@ -31,9 +32,12 @@ set /a ut=ut*86400+100%Hour%%%100*3600+100%Minute%%%100*60+100%Second%%%100
 endlocal & set "%1=%ut%" & goto :EOF
 
 :copy
-echo copy pictures to nas
-del /Q \\192.168.1.31\Stefan\web\*
-xcopy C:\Users\stefan\Documents\GitHub\carmenphotography\static\gallery\*.* \\192.168.1.31\Stefan\web
+echo delete all files in remote static/gallery folder
+ssh ubuntu@192.168.1.30 "rm /var/www/webApp/webApp/static/gallery/*"
+echo done
+echo.
 
+echo copy pictures from local to remote
+pscp C:\Users\stefan\Documents\GitHub\carmenphotography\static\gallery\* ubuntu@192.168.1.30:/var/www/webApp/webApp/static/gallery/
 
 pause
