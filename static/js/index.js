57213251e1d4246ba356e8803401d88af9a30674
StefanBa <stefanbaumann.b@gmail.com>
2021-09-15 13:01:16 +0200

    iris realized with transform

--- static/js/index.js
+++ static/js/index.js
@@ -11,80 +11,104 @@ let background = document.querySelector(".background");
 let svg = document.querySelector("svg");
 svg.setAttribute("viewBox", "-2.5 -2.5 5 5");
 
-//classes
+// //Animations
+// class Polygon {
+//   constructor(equationsX, equationsY) {
+//     this.node = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+//     this.node.setAttribute("style", "fill:white; stroke: black; stroke-width:0.03; stroke-opacity: 0.5; opacity:0.7; transition: all 1s ease 0s;");
+//     this.eqX = equationsX;
+//     this.eqY = equationsY;
+//     svg.appendChild(this.node);
+//   }
+
+//   update(a) {
+//     // console.log(act);
+//     // a = 3*a - 1.5;
+    
+//     if (a>0.5){
+//       a = a*(-4)+2.5;
+//     }
+//     console.log(a);
+//     // a = 1.5*a;
+//     let s = Math.sin(Math.PI/3);
+//     let c = 0.5 //sin(pi/3)
+//     let o = Math.cos(a);
+//     let i = Math.sin(a);
+//     let x = [];
+//     let y = [];
+//     let points = ""
+//     for (let w=0; w<3;w++){
+//       x[w]=parseFloat(eval(this.eqX[w]));
+//       y[w]=parseFloat(eval(this.eqY[w]));
+//       points += x[w] + "," + y[w] + " ";
+//     }
+//     this.node.setAttribute("points", points);
+//   };
+// }
+
+// let equationsX = [
+//   ["(-c*a)*o-s*a*i", "(1-c*a)*o-(s*a)*i", "(c-c*a)*o-(s+s*a)*i"],
+//   ["-a*o", "(c-a)*o-s*i", "(-c-a)*o-s*i"],
+//   ["(-c*a)*o-(-s*a)*i", "(-c-c*a)*o-(s-s*a)*i", "(-1-c*a)*o-(-s*a)*i"],
+//   ["(c*a)*o-(-s*a)*i", "(-1+c*a)*o-(-s*a)*i", "(-c+c*a)*o-(-s-s*a)*i"],
+//   ["a*o", "(-c+a)*o-(-s)*i", "(c+a)*o-(-s)*i"],
+//   ["(c*a)*o-(s*a)*i", "(1+c*a)*o-(s*a)*i", "(c+c*a)*o-(-s+s*a)*i"]
+// ];
+
+// let equationsY = [
+//   ["(-c*a)*i+(s*a)*o", "(1-c*a)*i+(s*a)*o", "(c-c*a)*i+(s+s*a)*o"],
+//   ["-a*i", "(c-a)*i+s*o", "(-c-a)*i+s*o"],
+//   ["(-c*a)*i+(-s*a)*o", "(-c-c*a)*i+(s-s*a)*o", "(-1-c*a)*i+(-s*a)*o"],
+//   ["(c*a)*i+(-s*a)*o", "(-1+c*a)*i+(-s*a)*o", "(-c+c*a)*i+(-s-s*a)*o"],
+//   ["a*i", "(-c+a)*i+(-s)*o", "(c+a)*i+(-s)*o"],
+//   ["(c*a)*i+(s*a)*o", "(1+c*a)*i+(s*a)*o", "(c+c*a)*i+(-s+s*a)*o"]
+// ];
+
+// let blades = []
+
+// for (let i = 0; i<6; i++){
+//   blades[i] = new Polygon(equationsX[i],equationsY[i]);
+// }
+
+//Animations
 class Polygon {
-  constructor(equationsX, equationsY) {
+  constructor(startRotation, translation) {
     this.node = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-    this.node.setAttribute("style", "fill:white; stroke: black; stroke-width:0.03; stroke-opacity: 0.5; opacity:0.7; transition: all 1s ease 0s;");
-    this.eqX = equationsX;
-    this.eqY = equationsY;
+    this.startRot = startRotation*Math.PI/3;
+    this.translates = translation;
+    this.node.setAttribute("points", "0,0 0.5,0.866 -0.5,0.866");
+    console.log(this.startRot);
+    this.node.setAttribute("style", "fill:white; stroke: black; stroke-width:0.03; stroke-opacity: 0.5; opacity:0.7; transition: all 0.5s ease 0s;" );
     svg.appendChild(this.node);
   }
 
   update(a) {
-    // console.log(act);
-    // a = 3*a - 1.5;
     a = 1.5*a;
-    let s = Math.sin(Math.PI/3);
-    let c = 0.5 //sin(pi/3)
-    let o = Math.cos(a);
-    let i = Math.sin(a);
-    let x = [];
-    let y = [];
-    let points = ""
-    for (let w=0; w<3;w++){
-      // console.log(parseFloat(eval(this.eqX[w])));
-      x[w]=parseFloat(eval(this.eqX[w]));
-      y[w]=parseFloat(eval(this.eqY[w]));
-      points += x[w] + "," + y[w] + " ";
-    }
-    this.node.setAttribute("points", points);
+    let xtranslates = this.translates[0]*a*20; //have to multiply by 20 because transform doesnt seem to work with user units, so use % instead
+    let ytranslates = this.translates[1]*a*20;
+    let angle = this.startRot;
+    this.node.style.transform = 
+      " rotate(" + a + "rad) \
+      translate(" + xtranslates + "%," + ytranslates + "%) \
+      rotate(" + angle + "rad)";
   };
 }
 
-let equationsX = [
-  ["(-c*a)*o-s*a*i", "(1-c*a)*o-(s*a)*i", "(c-c*a)*o-(s+s*a)*i"],
-  ["-a*o", "(c-a)*o-s*i", "(-c-a)*o-s*i"],
-  ["(-c*a)*o-(-s*a)*i", "(-c-c*a)*o-(s-s*a)*i", "(-1-c*a)*o-(-s*a)*i"],
-  ["(c*a)*o-(-s*a)*i", "(-1+c*a)*o-(-s*a)*i", "(-c+c*a)*o-(-s-s*a)*i"],
-  ["a*o", "(-c+a)*o-(-s)*i", "(c+a)*o-(-s)*i"],
-  ["(c*a)*o-(s*a)*i", "(1+c*a)*o-(s*a)*i", "(c+c*a)*o-(-s+s*a)*i"]
-];
-
-let equationsY = [
-  ["(-c*a)*i+(s*a)*o", "(1-c*a)*i+(s*a)*o", "(c-c*a)*i+(s+s*a)*o"],
-  ["-a*i", "(c-a)*i+s*o", "(-c-a)*i+s*o"],
-  ["(-c*a)*i+(-s*a)*o", "(-c-c*a)*i+(s-s*a)*o", "(-1-c*a)*i+(-s*a)*o"],
-  ["(c*a)*i+(-s*a)*o", "(-1+c*a)*i+(-s*a)*o", "(-c+c*a)*i+(-s-s*a)*o"],
-  ["a*i", "(-c+a)*i+(-s)*o", "(c+a)*i+(-s)*o"],
-  ["(c*a)*i+(s*a)*o", "(1+c*a)*i+(s*a)*o", "(c+c*a)*i+(-s+s*a)*o"]
-];
-
+let startRotations = [0, 1, 2, 3, 4, 5]; //is used as n*pi/3
+let translations = [[-1,0], [-0.5,-0.866], [0.5,-0.866], [1,0], [0.5,0.866], [-0.5,0.866]]; //0.5 is cos(pi/3) and 0.866 is sin(pi/3)
 let blades = []
 
 for (let i = 0; i<6; i++){
-  blades[i] = new Polygon(equationsX[i],equationsY[i]);
+  blades[i] = new Polygon(startRotations[i],translations[i]);
 }
 
 
-//script funtions
-function animateAll() {    
-  // subboarder.style.transform = "translate3d(0, " + offsety + "px, 0)";
-  let scrollableHeight = document.body.scrollHeight - window.innerHeight;
-  let currentScrollPos = window.pageYOffset;
-  let act = currentScrollPos / scrollableHeight; 
-  animateBackground(currentScrollPos)
-  animateSquares(act)
-  for (let i = 0; i<6; i++){
-    blades[i].update(act);
-  }
-
-}
-
+//parallax background
 function animateBackground(currentScrollPos){
   background.style.transform = "translate3d(0, -" + 0.5 * currentScrollPos + "px, 0)";
 }
 
+//squares animation
 function animateSquares(act){
   let subitemSize = (window.innerHeight / 100) * 3 + 5;
 
@@ -109,6 +133,21 @@ function animateSquares(act){
   }
 }
 
+function animateAll() {    
+  // subboarder.style.transform = "translate3d(0, " + offsety + "px, 0)";
+  let scrollableHeight = document.body.scrollHeight - window.innerHeight;
+  let currentScrollPos = window.pageYOffset;
+  let act = currentScrollPos / scrollableHeight; 
+  animateBackground(currentScrollPos)
+  animateSquares(act)
+  for (let i = 0; i<6; i++){
+    blades[i].update(act);
+  }
+}
+
+let idIndex = setInterval(animateAll, 10);
+
+//Backgroundimage always full bleed
 function resizeBackground() {
   if (x.matches || mobileBackgroundResize) {
     let viewX = main.clientWidth;
@@ -125,7 +164,7 @@ function resizeBackground() {
     if (backgroundX < viewX) {
       backgroundX = viewX;
       backgroundY = Math.ceil(viewX / ratio);
-      console.log("specialcase");
+      // console.log("specialcase");
     }
     background.style.backgroundSize = backgroundX + "px " + backgroundY + "px";
     background.style.backgroundPosition = "center center";
@@ -153,4 +192,4 @@ window.addEventListener("orientationchange", function (event) {
 });
 
 
-let idIndex = setInterval(animateAll, 10);
+
