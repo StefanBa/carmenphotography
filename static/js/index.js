fc0faad16d97f251ba54373c1f1f6f8c061dedd9
StefanBa <stefanbaumann.b@gmail.com>
2021-09-17 11:06:14 +0200

    dynamic iris

--- static/js/index.js
+++ static/js/index.js
@@ -10,129 +10,79 @@ let subboarder = document.querySelector(".subboarder");
 let background = document.querySelector(".background");
 let svg = document.querySelector("svg");
 svg.setAttribute("viewBox", "-2.5 -2.5 5 5");
-
-// //Animations
-// class Polygon {
-//   constructor(equationsX, equationsY) {
-//     this.node = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-//     this.node.setAttribute("style", "fill:white; stroke: black; stroke-width:0.03; stroke-opacity: 0.5; opacity:0.7; transition: all 1s ease 0s;");
-//     this.eqX = equationsX;
-//     this.eqY = equationsY;
-//     svg.appendChild(this.node);
-//   }
-
-//   update(a) {
-//     // console.log(act);
-//     // a = 3*a - 1.5;
-    
-//     if (a>0.5){
-//       a = a*(-4)+2.5;
-//     }
-//     console.log(a);
-//     // a = 1.5*a;
-//     let s = Math.sin(Math.PI/3);
-//     let c = 0.5 //sin(pi/3)
-//     let o = Math.cos(a);
-//     let i = Math.sin(a);
-//     let x = [];
-//     let y = [];
-//     let points = ""
-//     for (let w=0; w<3;w++){
-//       x[w]=parseFloat(eval(this.eqX[w]));
-//       y[w]=parseFloat(eval(this.eqY[w]));
-//       points += x[w] + "," + y[w] + " ";
-//     }
-//     this.node.setAttribute("points", points);
-//   };
-// }
-
-// let equationsX = [
-//   ["(-c*a)*o-s*a*i", "(1-c*a)*o-(s*a)*i", "(c-c*a)*o-(s+s*a)*i"],
-//   ["-a*o", "(c-a)*o-s*i", "(-c-a)*o-s*i"],
-//   ["(-c*a)*o-(-s*a)*i", "(-c-c*a)*o-(s-s*a)*i", "(-1-c*a)*o-(-s*a)*i"],
-//   ["(c*a)*o-(-s*a)*i", "(-1+c*a)*o-(-s*a)*i", "(-c+c*a)*o-(-s-s*a)*i"],
-//   ["a*o", "(-c+a)*o-(-s)*i", "(c+a)*o-(-s)*i"],
-//   ["(c*a)*o-(s*a)*i", "(1+c*a)*o-(s*a)*i", "(c+c*a)*o-(-s+s*a)*i"]
-// ];
-
-// let equationsY = [
-//   ["(-c*a)*i+(s*a)*o", "(1-c*a)*i+(s*a)*o", "(c-c*a)*i+(s+s*a)*o"],
-//   ["-a*i", "(c-a)*i+s*o", "(-c-a)*i+s*o"],
-//   ["(-c*a)*i+(-s*a)*o", "(-c-c*a)*i+(s-s*a)*o", "(-1-c*a)*i+(-s*a)*o"],
-//   ["(c*a)*i+(-s*a)*o", "(-1+c*a)*i+(-s*a)*o", "(-c+c*a)*i+(-s-s*a)*o"],
-//   ["a*i", "(-c+a)*i+(-s)*o", "(c+a)*i+(-s)*o"],
-//   ["(c*a)*i+(s*a)*o", "(1+c*a)*i+(s*a)*o", "(c+c*a)*i+(-s+s*a)*o"]
-// ];
-
-// let blades = []
-
-// for (let i = 0; i<6; i++){
-//   blades[i] = new Polygon(equationsX[i],equationsY[i]);
-// }
-
-
+let irisblades = 5;
 
 //Animations
 class Polygon {
-  constructor(startRotation, translation) {
-    this.startRot = startRotation*Math.PI/3;
-    this.translates = translation;
+  constructor(n, tot) {
+    this.n = n;
+    this.theta = 2*Math.PI/tot;
+    let startPoint = [Math.cos(this.theta/2), Math.sin(this.theta/2)];
     this.node = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-    this.node.setAttribute("points", "0,0 0.5,0.866 -0.5,0.866");
+    this.node.setAttribute("points", "0,0 " + startPoint[0] + "," + startPoint[1] + " " + startPoint[0] + "," + (-startPoint[1]));
     this.node.setAttribute("style", "fill:white; stroke: black; stroke-width:0.03; stroke-opacity: 0.5; opacity:0.7;" );
-    this.node.setAttribute("clip-path", "url(#clippy)");
     svg.appendChild(this.node);
   }
 
   update(a) {
-    // a = 1.3*a;
-    let xtranslates = this.translates[0]*a*20; //have to multiply by 20 because transform doesnt seem to work with user units, so use % instead
-    let ytranslates = this.translates[1]*a*20;
-    this.node.style.transform = " rotate(" + a + "rad) translate(" + xtranslates + "%," + ytranslates + "%) rotate(" + this.startRot + "rad) ";
+    this.node.style.transform = "rotate(" + a + "rad)  rotate(" + this.n*this.theta + "rad) translate(0," + (a*20) + "%)";
   };
+
+  clear(){
+    svg.removeChild(this.node);
+  }
 }
 
-let startRotations = [0, 1, 2, 3, 4, 5]; //is used as n*pi/3
-let translations = [[-1,0], [-0.5,-0.866], [0.5,-0.866], [1,0], [0.5,0.866], [-0.5,0.866]]; //0.5 is cos(pi/3) and 0.866 is sin(pi/3)
-let blades = []
+class Iris {
+  constructor(){
+    this.blades = [];
+    this.n_blades = 0;
+    let irisCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    irisCircle.setAttribute("r","1");
+    irisCircle.setAttribute("style", "fill-opacity:0; stroke: black; stroke-width:0.03; stroke-opacity: 1;" );
+    svg.appendChild(irisCircle);
+  }
 
-for (let i = 0; i<6; i++){
-  blades[i] = new Polygon(startRotations[i],translations[i]);
+  init(startblades){
+    if(startblades){
+      this.n_blades = startblades;
+    }
+    
+    for (let i = 0; i<this.n_blades; i++){
+      this.blades[i] = new Polygon(i,this.n_blades);
+    }
+  }
+
+  update(a){
+    for (let i = 0; i<this.n_blades; i++){
+      this.blades[i].update(a);
+    }
+  }
+
+  clear(){
+    for (let i = 0; i<this.n_blades; i++){
+      this.blades[i].clear();
+    }
+    this.blades = [];
+  }
+
+  increse(){
+    this.n_blades++;
+  }
 }
 
-let irisCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-irisCircle.setAttribute("r","1");
-irisCircle.setAttribute("style", "fill-opacity:0; stroke: black; stroke-width:0.03; stroke-opacity: 1;" );
-svg.appendChild(irisCircle);
+let iris = new Iris();
+iris.init(5);
+
+svg.addEventListener("click", function(){
+  iris.clear();
+  iris.increse();
+  iris.init();
+});
 
 //parallax background
 function animateBackground(currentScrollPos){
   background.style.transform = "translate3d(0, -" + 0.5 * currentScrollPos + "px, 0)";
-}
-
-//squares animation
-function animateSquares(act){
-  let subitemSize = (window.innerHeight / 100) * 3 + 5;
-
-  let animationStyle = {
-  offx: [-1.5, -0.5, 0.5, 1.5, -1.5, -0.5, 0.5, 1.5, -1.5, -0.5, 0.5, 1.5, -1.5, -0.5, 0.5, 1.5],
-  offy: [1.5, 1.5, 1.5, 1.5, 0.5, 0.5, 0.5, 0.5, -0.5, -0.5, -0.5, -0.5, -1.5, -1.5, -1.5, -1.5],
-  speedx: [-0.6, -0.3, 0.4, 0.8, -0.5, -0.1, 0.3, 0.9, -0.8, -0.3, 0.2, 0.3, -0.8, -0.7, 0.4, 0.8],
-  speedy: [0.8, 0.7, 0.5, 0.9, 0.5, 0.3, 0.2, 0.6, -0.3, -0.6, -0.2, -0.5, -0.6, -0.7, -0.9, -0.8],
-  opacity: [0.4, 0.5, 0.6, 0.3, 0.5, 0.8, 0.2, 0.7, 0.6, 0.5, 0.9, 0.6, 0.2, 0.3, 0.5, 0.7],
-};
-
-  for (var i = 0; i < subitems.length; i++) {
-    let posx = Math.round(animationStyle.offx[i] * subitemSize + animationStyle.speedx[i] * act * 0.15 * window.innerWidth - subitemSize/2);
-    let posy = Math.round(
-    animationStyle.offy[i] * subitemSize + animationStyle.speedy[i] * act * 0.2 * window.innerHeight - subitemSize/2);
-    let opa = Math.round((-(1 - animationStyle.opacity[i]) * act + 0.8) * 100) / 100;
-    subitems[i].style.transform = "translate3d(" + posx + "px," + posy + "px, 0)";
-    subitems[i].style.opacity = opa;
-    let size = Math.round((window.innerHeight * Math.abs(animationStyle.speedx[i]) * act * 0.005 + 3) * 100) / 100;
-    subitems[i].style.width = size + "vh";
-    subitems[i].style.height = size + "vh"; 
-  }
 }
 
 function animateAll() {    
@@ -142,9 +92,7 @@ function animateAll() {
   let act = currentScrollPos / scrollableHeight; 
   animateBackground(currentScrollPos)
   animateSquares(act)
-  for (let i = 0; i<6; i++){
-    blades[i].update(act);
-  }
+  iris.update(act);
 }
 
 let idIndex = setInterval(animateAll, 10);
